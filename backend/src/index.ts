0d1e601757f817add6c84cf339a0d036d1002e32
cheeter_Lee <johnson991210@gmail.com>
2023-04-05 10:47:26 +0800

    connect to psql database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,29 @@
+import "reflect-metadata"
 import express from "express"
+import { DataSource } from "typeorm"
+import { join } from "path"
+import { __prod__ } from "./constants"
+import { User } from "./entities/User"
 
 const main = async () => {
+	const dataSource = new DataSource({
+		type: "postgres",
+		host: "localhost",
+		port: 5432,
+		database: "todosheet",
+		entities: [join(__dirname, "./entities/*.*")],
+		logging: !__prod__,
+		synchronize: !__prod__,
+	})
+
+	await dataSource.initialize()
+
+	const user = await User.create({
+		name: "John Doe",
+	}).save()
+
+    console.log(user)
+
 	const app = express()
 	app.get("/", (_, res) => {
 		res.send("Hello World")
