cb01784ecc949729b92ce1a9825f5b436b2bbd01
cheeter_Lee <johnson991210@gmail.com>
2023-04-05 21:26:04 +0800

    polka server setup

--- todosheet/src/authenticate.ts
+++ todosheet/src/authenticate.ts
@@ -1,9 +1,28 @@
 import * as vscode from 'vscode'
 import { apiBaseUrl } from './constants'
+import * as polka from 'polka'
 
 export const authenticate = () => {
-    vscode.commands.executeCommand(
-        "vscode.open",
-        vscode.Uri.parse(`${apiBaseUrl}/auth/github`)
-    )
+    const app = polka()
+
+    app.get(`/auth/:token`,  async (req, res) => {
+        const { token } = req.params
+        if (!token) {
+            res.end('<h1>Something went wrong</h1>')
+            return
+        }
+        console.log(token)
+        res.end('<h1>Auth was successful, you can close this now</h1>')
+    })
+
+    app.listen(54321, (err: Error) => {
+        if (err) {
+            vscode.window.showErrorMessage(err.message)
+        } else {
+            vscode.commands.executeCommand(
+                "vscode.open",
+                vscode.Uri.parse(`${apiBaseUrl}/auth/github`)
+            )
+        }
+    })
 }
